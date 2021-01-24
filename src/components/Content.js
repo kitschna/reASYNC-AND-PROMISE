a2bdefe3c873265c22d73d3c718687813ce215aa
kitschna <gichang.ee@gmail.com>
2021-01-24 23:39:58 +0900

    complete 18

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 
 class Content extends Component {
   render() {
+    // this.props.title = 'hi' // component 안에서 자기로 전달 된 props의 값은 금지로 되어있음. 단, 컴퍼넌트 밖에서 바꾸는건 허용됨
+    console.log('Content render')
     return (
       <article>
         <h2>{this.props.title}</h2>
